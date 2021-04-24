00aff3b11782e00de4241ac5c9ca1ec9ec82bcf5
v.karmazin <v.karmazin@tinkoff.ru>
2021-04-24 17:48:21 +0300

    ADD: echo to db

--- app/db/settings.py
+++ app/db/settings.py
@@ -1,17 +1,17 @@
 import sqlalchemy as sa
-
-from pydantic import BaseSettings, AnyUrl
+from pydantic import AnyUrl, BaseSettings
 
 
 class DBSettings(BaseSettings):
     url: AnyUrl = 'postgresql://postgres:postgres@localhost:15432/kabanchiki'
+    echo: int = 1
 
     class Config:
         env_prefix = 'DB_'
 
     @property
     def engine(self) -> sa.engine.Engine:
-        return sa.create_engine(str(self.url))
+        return sa.create_engine(str(self.url), echo=self.echo)
 
     def setup_db(self) -> None:
         from app.db.base import metadata
