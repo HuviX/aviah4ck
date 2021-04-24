679fb6d92c47d716ef5c3871789e484fd05c0305
v.karmazin <v.karmazin@tinkoff.ru>
2021-04-24 19:24:14 +0300

    UPD: dataset loading

--- app/db/dataset.py
+++ app/db/dataset.py
@@ -1,10 +1,21 @@
+from enum import Enum
+
 import sqlalchemy as sa
 
 from app.db.base import Base
 
 
+class DatasetType(Enum):
+    TRAIN = 'train'
+    TEST = 'test'
+    UNLABELLED = 'unlabelled'
+
+
 class Dataset(Base):
     __tablename__ = 'dataset'
 
-    name = sa.Column(sa.Text, nullable=False)
+    name = sa.Column(sa.String, nullable=False)
     description = sa.Column(sa.Text, nullable=True)
+    train_count = sa.Column(sa.Integer, nullable=False, default=0)
+    test_count = sa.Column(sa.Integer, nullable=False, default=0)
+    unlabelled_count = sa.Column(sa.Integer, nullable=False, default=0)
--- app/pages/dataset.py
+++ app/pages/dataset.py
@@ -1,10 +1,14 @@
 import logging
+import tarfile
 import time
+from pathlib import Path
 
 import streamlit as st
+from sqlalchemy import desc
 from sqlalchemy.orm import Query
 
 from app import db
+from app.db.dataset import DatasetType
 from app.db.utils import create_session, get_dataframe_from_query
 from app.pages.utils import Page, render_horizontal_pages
 from app.state import session_state
@@ -24,43 +28,90 @@ def app():
 
 def create_dataset():
     st.markdown(
-        '*Необходимо дать название датасету и загрузить данные с метками или данные без меток*'
+        f"""
+        - *Необходимо дать название датасету и загрузить данные с метками или данные без меток*
+        - *Данные в архиве формата `.tar`*
+        - *Формат данных c метками*
+        ```
+        ├── {DatasetType.TRAIN}
+        │   ├── img0.png
+        │   ├── img1.png
+        │   ├── img2.png
+        │   └── labels.csv
+        └── {DatasetType.TEST}
+            ├── img3.png
+            ├── img4.png
+            ├── img5.png
+            └── labels.csv
+        ```
+        - *Формат файла `labels.csv`*
+        ```
+        image	x	y	width	height
+        0.png	70	19	100	    111
+        1.png	108	91	89	    82
+        2.png	90	19	115	    181
+        ```
+        - *Формат данных без меток*
+        ```
+        └── {DatasetType.UNLABELLED}
+            ├── img0.png
+            ├── img1.png
+            └── img2.png
+        ```
+    """
     )
     name = st.text_input('Название')
     description = st.text_area('Описание')
-
-    st.markdown('*Данные с метками*')
-    left_column, right_column = st.beta_columns(2)
-    dataset = left_column.file_uploader('Архив с обучающими данными')
-    test_dataset = right_column.file_uploader('Архив с тестовыми данными')
-
-    st.markdown('*Данные без меток*')
-    unlabelled_dataset = st.file_uploader('Архив с данными без разметки')
+    dataset = st.file_uploader('Архив c данными', type=['tar', 'tar.gz', 'tar.xz'])
 
     save_button = st.button('Сохранить')
-
     if save_button:
-        if _save_dataset(name, description, dataset, test_dataset, unlabelled_dataset):
+        if _save_dataset(name, description, dataset):
             st.info('Сохранено')
             time.sleep(1)
             session_state.subpage = None
             st.experimental_rerun()
 
 
-def _save_dataset(name, description, dataset, test_dataset, unlabelled_dataset):
+def _save_dataset(name, description, dataset):
     if not name:
         st.error('Необходимо задать название датасету')
         return False
-    if dataset and not test_dataset:
-        st.error('Необходимо загрузить тестовые данные')
-        return False
-    if not dataset and not test_dataset and not unlabelled_dataset:
+
+    if not dataset:
         st.error('Необходимо загрузить данные с метками или без меток')
         return False
 
     with create_session() as session:
-        dataset = db.Dataset(name=name, description=description)  # noqa
-        session.add(dataset)
+        record = db.Dataset(name=name, description=description)  # noqa
+        session.add(record)
+        session.flush()
+
+        try:
+            uploaded_file = dataset
+            path = Path(f'data/{record.id}/temp.tar.xz')
+            path.parent.mkdir(exist_ok=True)
+
+            with open(path, 'wb') as f:
+                f.write(uploaded_file.read())
+
+            with tarfile.open(path) as f:
+                f.extractall(path=f'data/{record.id}/')
+
+            record.train_count = len(
+                list((path.parent / DatasetType.TRAIN.value).glob('*.png'))
+            )
+            record.test_count = len(
+                list((path.parent / DatasetType.TEST.value).glob('*.png'))
+            )
+            record.unlabelled_count = len(
+                list((path.parent / DatasetType.UNLABELLED.value).glob('*.png'))
+            )
+
+        except Exception as e:
+            st.exception(e)
+            return False
+
     return True
 
 
@@ -81,13 +132,17 @@ def show_dataset():
         get_dataframe_from_query(
             Query(
                 [
-                    db.Dataset.id,
                     db.Dataset.name.label('Название датасета'),
                     db.Dataset.description.label('Описание'),
+                    db.Dataset.train_count.label('Обучающих'),
+                    db.Dataset.test_count.label('Тестовых'),
+                    db.Dataset.unlabelled_count.label('Сырых'),
                     db.Dataset.created_at.label('Дата создания'),
-                    db.Dataset.updated_at.label('Дата последнего изменения'),
+                    db.Dataset.updated_at.label('Последнее изменение'),
                 ]
             )
+            .order_by(desc(db.Dataset.updated_at))
+            .limit(50)
         )
     )
 
