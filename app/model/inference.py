9c85bac4ca76c709dab011cd3cc25d938a71a355
v.karmazin <v.karmazin@tinkoff.ru>
2021-04-25 13:35:32 +0300

    sdlfkgh

--- app/model/inference.py
+++ app/model/inference.py
@@ -1,33 +1,31 @@
-import torch
-import torchvision
-import os
 import os
 import random
+import warnings
+from typing import List
+
+import addict
+import albumentations as A
+import cv2
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import yaml
-import warnings
-warnings.filterwarnings('ignore')
-import addict
-import cv2
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import torch
 import torchvision
+import yaml
+from albumentations.pytorch.transforms import ToTensorV2
 from torchvision import transforms
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-import albumentations as A
-from albumentations.pytorch.transforms import ToTensorV2
-from typing import List
+warnings.filterwarnings('ignore')
 
 
 
 # canny filter crop to remove background
 def crop_canny(img):
-    blurred = cv2.blur(img, (3,3))
+    blurred = cv2.blur(img, (3, 3))
     canny = cv2.Canny(blurred, 50, 200)
-    pts = np.argwhere(canny>0)
+    pts = np.argwhere(canny > 0)
     y1, x1 = pts.min(axis=0)
     y2, x2 = pts.max(axis=0)
     cropped = img[y1:y2, x1:x2]
@@ -37,24 +35,26 @@ def crop_canny(img):
 def get_object_detection_model(num_classes):
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes) 
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     return model
 
 
 def apply_nms(orig_prediction, iou_thresh=0.3):
     # torchvision returns the indices of the bboxes to keep
-    keep = torchvision.ops.nms(orig_prediction['boxes'], orig_prediction['scores'], iou_thresh)
-    
+    keep = torchvision.ops.nms(
+        orig_prediction['boxes'], orig_prediction['scores'], iou_thresh
+    )
+
     final_prediction = orig_prediction
     final_prediction['boxes'] = final_prediction['boxes'][keep]
     final_prediction['scores'] = final_prediction['scores'][keep]
     final_prediction['labels'] = final_prediction['labels'][keep]
-    
+
     return final_prediction
+
 
 def torch_to_pil(img):
     return transforms.ToPILImage()(img).convert('RGB')
-
 
 
 def test_time_transform(image, w=224, h=224):
@@ -62,26 +62,32 @@ def test_time_transform(image, w=224, h=224):
     img_res = cv2.resize(img_rgb, (w, h), cv2.INTER_AREA)
     img_res /= 255.0
     return ToTensorV2()(image=img_res)['image']
-    
 
-def random_window_prediction(model, path: str, n_crops: int = 100, k: int = 1, canny_crop: bool = False,  device=torch.device('cpu')) -> np.ndarray:
+
+def random_window_prediction(
+    model,
+    path: str,
+    n_crops: int = 100,
+    k: int = 1,
+    canny_crop: bool = False,
+    device=torch.device('cpu'),
+) -> np.ndarray:
     src_img = cv2.imread(path)
     if canny_crop:
         src_img = crop_canny(src_img)
-    
+
     ## define a 100 of crops
     crops = {}
     for i in range(n_crops):
         size = np.random.randint(440, 512)
-        y1 = np.random.randint(w//3, h - size)
-        x1 = np.random.randint(h//3, w - size)
+        y1 = np.random.randint(w // 3, h - size)
+        x1 = np.random.randint(h // 3, w - size)
         x2 = x1 + size
         y2 = y1 + size
-        d = {'image': src_img[y1: y2, x1: x2],
-             'coords': (y1, y2, x1, x2)}
+        d = {'image': src_img[y1:y2, x1:x2], 'coords': (y1, y2, x1, x2)}
         crops[i] = d
 
-    #Model outputs a dictionary with keys 'scores', 'boxes', 'labels'
+    # Model outputs a dictionary with keys 'scores', 'boxes', 'labels'
     # scores_i corresponds to confidence of boxes_i.
     # Ex: {scores: [0.1, 0.43, 0.2, ...],
     #      bbox: [(x1, x2, w, h), ...],
@@ -89,7 +95,7 @@ def random_window_prediction(model, path: str, n_crops: int = 100, k: int = 1, c
     #    }
 
     # Get the maximum confidence for bbox across image(crop).
-    # Store these values across all n_crops in order to 
+    # Store these values across all n_crops in order to
     # choose the most confident one. (or three as in this function)
     boxes = []
     scores = []
@@ -104,7 +110,7 @@ def random_window_prediction(model, path: str, n_crops: int = 100, k: int = 1, c
         boxes.append(box)
         scores.append(max_score)
     # pick 3 most confident boxes for all n_crops
-    top = torch.argsort(torch.tensor(scores), descending=True)[: k]
+    top = torch.argsort(torch.tensor(scores), descending=True)[:k]
 
     for ind in top:
         ind = ind.item()
@@ -117,27 +123,34 @@ def random_window_prediction(model, path: str, n_crops: int = 100, k: int = 1, c
         cv2.rectangle(crop, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
     return src_img
 
-def sliding_window_prediction(model, path: str, windows: List[int] = [224], canny_crop: bool = False, k: int = 1, device=torch.device('cpu')):
+
+def sliding_window_prediction(
+    model,
+    path: str,
+    windows: List[int] = [224],
+    canny_crop: bool = False,
+    k: int = 1,
+    device=torch.device('cpu'),
+):
     src_img = cv2.imread(path)
     im = src_img
     if canny_crop:
-        im, cy, cx  = crop_canny(src_img)
- 
-    imgheight=im.shape[0]
-    imgwidth=im.shape[1]
-    
+        im, cy, cx = crop_canny(src_img)
+
+    imgheight = im.shape[0]
+    imgwidth = im.shape[1]
+
     for window in windows:
         crops = dict()
-        M = imgheight//(imgheight//window)
-        N = imgwidth//(imgwidth//window)
+        M = imgheight // (imgheight // window)
+        N = imgwidth // (imgwidth // window)
         i = 0
-        for y in range(0,imgheight,M):
+        for y in range(0, imgheight, M):
             for x in range(0, imgwidth, N):
                 y1 = y + M
                 x1 = x + N
-                tiles = im[y:y+M, x:x+N]
-                d = {'image': im[y:y+M, x:x+N],
-                     'coords': (y, y+M, x, x+N)}
+                tiles = im[y : y + M, x : x + N]
+                d = {'image': im[y : y + M, x : x + N], 'coords': (y, y + M, x, x + N)}
                 crops[i] = d
                 i += 1
 
@@ -154,7 +167,7 @@ def sliding_window_prediction(model, path: str, windows: List[int] = [224], cann
             boxes.append(box)
             scores.append(max_score)
 
-        top = torch.argsort(torch.tensor(scores), descending=True)[: k]
+        top = torch.argsort(torch.tensor(scores), descending=True)[:k]
         for ind in top:
             ind = ind.item()
             box = boxes[ind]
@@ -164,7 +177,7 @@ def sliding_window_prediction(model, path: str, windows: List[int] = [224], cann
             xmax = box[2] + xmin
             ymax = box[3] + ymin
             cv2.rectangle(crop, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
-        src_img[cy[0]: cy[1], cx[0]: cx[1]] = im
+        src_img[cy[0] : cy[1], cx[0] : cx[1]] = im
     return src_img
 
 
@@ -175,7 +188,7 @@ def main(**kwargs):
     n_crops = kwargs['n_crops']
     path_out = kwargs['path_out']
 
-    #draw top k boxes
+    # draw top k boxes
     top_k = kwargs['top_k']
     canny_crop = kwargs['canny_crop']
     model_path = kwargs['model_path']
@@ -187,12 +200,13 @@ def main(**kwargs):
     model.eval()
     model.to(device)
 
-    if prediction_type == 'window'
+    if prediction_type == 'window':
         res = sliding_window_prediction(model, path, canny_crop=True, k=3)
     else:
         res = random_window_prediction(model, path, n_crops, top_k)
     cv2.imwrite(path_out, res)
-    print("Done")
+    print('Done')
+
 
 # ---- Usage ----
 # from inference import main
